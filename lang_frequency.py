28d521ccfca6c1bb7ade977fe8aaa46883a62100
buktop <buktop>
2018-01-14 00:27:55 +0900

    Первый релиз

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,63 @@
+import argparse
+import re
 
 
-def load_data(filepath):
-    pass
+def get_argv():
+    parser = argparse.ArgumentParser(
+        description="Частотный анализ слов в тексте"
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="Путь до текстового файла"
+    )
+    return parser.parse_args().file
+
+
+def load_data(file_path):
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.read()
+    except (FileNotFoundError, UnicodeDecodeError):
+        return None
 
 
 def get_most_frequent_words(text):
-    pass
+    text = text.lower()
+    compile_re = re.compile(r"\W+", re.UNICODE)
+    word_list = re.split(compile_re, text)
+    # Если будут не алфовитно-цифровой символ в начале текста или в конце,
+    # то первый и последний элемент списка будет ""
+    if word_list[0] == "":
+        word_list.pop(0)
+    if word_list[-1] == "":
+        word_list.pop()
+    words = set(word_list)
+    frequent_words = []
+    for word in words:
+        coincidence = 1
+        number_word = sum(
+            [coincidence for x in word_list if x == word]
+        )
+        frequent_words.append((word, number_word))
+    frequent_words = sorted(
+        frequent_words,
+        key=lambda x: x[1],
+        reverse=True
+    )
+    return frequent_words
 
 
 if __name__ == '__main__':
-    pass
+    file_path = get_argv()
+    text = load_data(file_path)
+    if text:
+        frequent_words = get_most_frequent_words(text)
+        number = 1
+        for frequent_word in frequent_words[:10]:
+            print("{0}. '{1}' упоменаеться {2} раз".format(
+                number, frequent_word[0], frequent_word[1]
+            ))
+            number += 1
+    else:
+        print("Путь до файла введен не верно!")
