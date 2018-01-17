e3efb8453113dbe9d55e65207c4fee81da0785d4
buktop <buktop>
2018-01-17 22:13:51 +0900

    Исправлены ошибки от 16.01.2018г

--- lang_frequency.py
+++ lang_frequency.py
@@ -12,7 +12,7 @@ def get_argv():
         "--file",
         help="Путь до текстового файла"
     )
-    return parser.parse_args().file
+    return parser.parse_args()
 
 
 def load_data(file_path):
@@ -26,23 +26,21 @@ def load_data(file_path):
 def get_most_frequent_words(text):
     text = text.lower()
     word_list = re.findall(r"\w+", text)
-    # Колличество популярных слов которые должна выводит функция
     number_most_frequent_words = 10
     most_frequent_words = collections.Counter(word_list)
-    ten_frequent_words = most_frequent_words.most_common(
+    frequent_words = most_frequent_words.most_common(
         number_most_frequent_words
     )
-    return ten_frequent_words
+    return frequent_words
 
 
-if __name__ == '__main__':
-    file_path = get_argv()
+if __name__ == "__main__":
+    file_path = get_argv().file
     text = load_data(file_path)
     if text:
-        ten_frequent_words = get_most_frequent_words(text)
-        print("="*10+"10 самых популярных слов в тексте"+"="*10)
-        print ("В тексте встречается: ")
-        for word, count in ten_frequent_words:
-            print("'{0}' - {1} раз".format(word, count))
+        frequent_words = get_most_frequent_words(text)
+        print("10 самых популярных слов в тексте:")
+        for word, count in frequent_words:
+            print("'{}' - {} слов".format(word, count))
     else:
         print("Путь до файла введен не верно!")
